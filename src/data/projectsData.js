ceac2c9d1eaa9293549bea7a1af2ec8be6aa920a
Shruti2-0Thakur <109304948+Shruti2-0Thakur@users.noreply.github.com>
2024-07-18 22:40:31 +0530

    first commit

---
+++ src/data/projectsData.js
@@ -0,0 +1,49 @@
+import audio from "../assets/png/Project/audio.jpg";
+import elearn from "../assets/png/Project/e-learn.jpg";
+import hiring from '../assets/png/Project/hiring.jpg';
+import port from '../assets/png/Project/portfolio.png';
+export const projectsData = [
+
+  {
+    id: 1,
+    projectName: "HR Management System",
+    projectDesc:
+    "Streamline hiring with our HR platform: unlimited job postings, 95% candidate accuracy, automated scheduling.",
+    tags: [".Net, c#", "HTML & CSS", "API", "SQL"],
+    code: "https://github.com/Shruti2-0Thakur/HR",
+    demo: "",
+    image: hiring,
+  },
+  {
+    id: 1,
+    projectName: "Dream Square",
+    projectDesc:
+    "Experience interactive learning with our platform: personalized courses, secure payments, engaging blog content, and streamlined account management.",
+    tags: ["React.JS", "Node.js", "Rest API", "SQL"],
+    code: "https://github.com/Shruti2-0Thakur/DreamSquare",
+    demo: "",
+    image: elearn,
+  },
+  {
+    id: 2,
+    projectName: "VTA (video to audio)",
+    projectDesc:
+    "Boost efficiency with our user-friendly MP4 to MP3 converter. Upload MP4s or YouTube links, enjoy faster downloads, and increase user engagement.",
+    tags: ["HTML", "CSS", "Javascript", "Node.js"],
+    code: "https://github.com/Shruti2-0Thakur/VTA",
+    demo: "",
+    image: audio,
+  },
+  {
+    id: 1,
+    projectName: "Portfolio API",
+    projectDesc:
+    "This project is an API implementation of a portfolio using MongoDB for dynamic content management, supporting real-time updates and CRUD operations.",
+    tags: ["MongoDB", "node.js", "Express.js"],
+    code: "https://github.com/Shruti2-0Thakur/api_portfolio/tree/main",
+    demo: "",
+    image: port,
+  },
+
+];
+
---
+++ src/data/socialsData.js
@@ -0,0 +1,8 @@
+export const socialsData = {
+  github: "https://github.com/Shruti2-0Thakur",
+
+  linkedIn: "https://www.linkedin.com/in/shrutithakur20/",
+  instagram: "https://www.instagram.com/_thakur_._shruti_/?igsh=MTd2ZWw1NWxmOWV1MA%3D%3D",
+  leetCode: "https://leetcode.com/u/Shruti__20/",
+  
+};
